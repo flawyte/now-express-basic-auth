02224a6d551b8f67b1b8a2b97eb98d0bc841f77c
Mickaël Allonneau <mickael.allonneau@gmail.com>
2019-03-07 14:16:27 +0100

    Basic auth for all pages (guest:guest).

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const basicAuth = require('express-basic-auth');
+
+/*
+ *
+ */
+
 const app = express();
 
+app.use(basicAuth({
+  challenge: true,
+  realm: 'now-express-basic-auth',
+  users: { 'guest': 'guest' },
+  unauthorizedResponse: 'Restricted area. Please login (guest:guest).'
+}));
 app.use(express.static('src'));
+
+/*
+ *
+ */
 
 app.listen(4444, () => console.log('Listening on port 4444...'));
