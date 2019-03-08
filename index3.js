d449a496a00cf9d04017dfc291cadd926827042f
Mickaël Allonneau <mickael.allonneau@gmail.com>
2019-03-08 12:57:20 +0100

    index3.js with serve-static package.

---
+++ index3.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const basicAuth = require('express-basic-auth');
+const serveStatic = require('serve-static');
+
+/*
+ *
+ */
+
+const app = express();
+
+app.use(basicAuth({
+  challenge: true,
+  realm: 'now-express-basic-auth',
+  users: { 'guest': 'guest' },
+  unauthorizedResponse: 'Restricted area. Please login (guest:guest).'
+}));
+
+app.use(serveStatic(__dirname + '/src'));
+
+/*
+ *
+ */
+
+app.listen(4444, () => console.log('Listening on port 4444...'));
